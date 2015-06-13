341c2bcc444f1d66ed1eea5ac12474aecc88fd59
Mike <mike@flomp.net>
2015-06-13 18:45:48 +0100

    Remove status bar for now

--- main.cpp
+++ main.cpp
@@ -151,7 +151,6 @@ bool MyApp::OnInit()
 MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
         : wxFrame(NULL, wxID_ANY, title, pos, size)
 {
-    CreateStatusBar();
 }
 
 void MyFrame::OnExit(wxCommandEvent& event)
