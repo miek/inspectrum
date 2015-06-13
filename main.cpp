51de946e3dba805218fdd031529181df6d02adb3
Mike <mike@flomp.net>
2015-06-13 13:40:57 +0100

    Draw in scrolled window, process more of test file

--- main.cpp
+++ main.cpp
@@ -13,21 +13,20 @@
 class wxImagePanel : public wxScrolled<wxPanel>
 {
 private:
-    wxBitmap *image;
+    float *image_data;
 
 public:
-    wxImagePanel(wxFrame *parent, wxBitmap *image);
+    wxImagePanel(wxFrame *parent, float *image_data);
 
     void paintEvent(wxPaintEvent &evt);
     void paintNow();
 
-    void render(wxDC &dc);
+    void OnDraw(wxDC &dc);
 
     wxDECLARE_EVENT_TABLE();
 };
 
 wxBEGIN_EVENT_TABLE(wxImagePanel, wxScrolled<wxPanel>)
-    EVT_PAINT(wxImagePanel::paintEvent)
 wxEND_EVENT_TABLE()
 
 class MyApp: public wxApp
@@ -53,43 +52,57 @@ wxEND_EVENT_TABLE()
 
 wxIMPLEMENT_APP(MyApp);
 
-wxImagePanel::wxImagePanel(wxFrame *parent, wxBitmap *image) : wxScrolled<wxPanel>(parent)
+wxImagePanel::wxImagePanel(wxFrame *parent, float *image_data) : wxScrolled<wxPanel>(parent)
 {
-    this->image = image;
+    this->image_data = image_data;
+    SetScrollRate(10, 10);
+    SetVirtualSize(FFT_SIZE, 25885879);
 }
 
-void wxImagePanel::paintEvent(wxPaintEvent &evt)
+void wxImagePanel::OnDraw(wxDC &dc)
 {
-    wxPaintDC dc(this);
-    render(dc);
+    int x, y;
+    int xunit, yunit;
+    int width, height;
+    GetViewStart(&x, &y);
+    GetScrollPixelsPerUnit(&xunit, &yunit);
+    GetClientSize(&width, &height);
+
+    x *= xunit;
+    y *= yunit;
+
+    wxBitmap image(FFT_SIZE, height, 24);
+    wxNativePixelData data(image);
+    wxNativePixelData::Iterator pix(data);
+
+    int row_start = y * FFT_SIZE;
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < FFT_SIZE; j++) {
+            pix.Blue() = image_data[row_start + j] * -2;
+            pix.Red() = 0;
+            pix.Green() = 0;
+            pix++;
+        }
+        row_start += FFT_SIZE;
+    }
+
+    dc.DrawBitmap(image, 0, y, false);
 }
 
-void wxImagePanel::paintNow()
-{
-    wxClientDC dc(this);
-    render(dc);
-}
-
-void wxImagePanel::render(wxDC &dc)
-{
-    dc.DrawBitmap(*image, 0, 0, false);
-}
-
-wxBitmap image(FFT_SIZE, 252792, 24);
 
 bool MyApp::OnInit()
 {
     wxInitAllImageHandlers();
-
-    MyFrame *frame = new MyFrame( "Hello World", wxPoint(50, 50), wxSize(1024, 500) );
-
     wxBoxSizer *sizer = new wxBoxSizer(wxHORIZONTAL);
+    MyFrame *frame = new MyFrame( "merry supernova", wxPoint(50, 50), wxSize(1024, 500) );
 
     int fd;
     fftwf_complex *input;
+    float *output;
 
     fd = open("/home/mike/projects/hubsan/name-f2.431000e+09-s2.000000e+07-t20150524002153.cfile", O_RDONLY);
-    input = (fftwf_complex*)mmap(NULL, 2022340, PROT_READ, MAP_SHARED, fd, 0);
+    input = (fftwf_complex*)mmap(NULL, 2070870320, PROT_READ, MAP_SHARED, fd, 0);
+    output = (float*)malloc(258858790 * sizeof(float));
 
     fftwf_complex *in, *out;
     fftwf_plan p;
@@ -98,9 +111,8 @@ bool MyApp::OnInit()
     out = (fftwf_complex*)fftwf_malloc(sizeof(fftwf_complex) * FFT_SIZE);
     p = fftwf_plan_dft_1d(FFT_SIZE, in, out, FFTW_FORWARD, FFTW_MEASURE);
 
-    wxNativePixelData data(image);
-    wxNativePixelData::Iterator pix(data);
-    for (int i = 0; i < 252792 - FFT_SIZE; i += FFT_SIZE) {
+    float *output_ptr = output;
+    for (int i = 0; i < 25885879 - FFT_SIZE; i += FFT_SIZE) {
         memcpy(in, input[i], sizeof(fftwf_complex) * FFT_SIZE);
         fftwf_execute(p);
 
@@ -110,17 +122,16 @@ bool MyApp::OnInit()
             im = out[j][1];
             mag = sqrt(re * re + im * im) / FFT_SIZE;
             magdb = 10 * log(mag);
-            pix.Blue() = magdb;
-            pix++;
+            *output_ptr = magdb;
+            output_ptr++;
         }
     }
 
     fftwf_destroy_plan(p);
     fftwf_free(in); fftwf_free(out);
 
-    wxImagePanel *impanel = new wxImagePanel(frame, &image);
-    sizer->Add(impanel, 1, wxEXPAND);
-
+    wxImagePanel *impanel = new wxImagePanel(frame, output);
+    sizer->Add(impanel, 1, wxALL | wxEXPAND, 0);
     frame->SetSizer(sizer);
 
     frame->Show( true );
