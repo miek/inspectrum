af50dd3c8fc0116bf83527fdb16086f0e832f485
Mike Walters <mike@flomp.net>
2015-07-19 01:40:13 +0100

    Set parent widgets

--- spectrogramcontrols.cpp
+++ spectrogramcontrols.cpp
@@ -4,20 +4,20 @@
 SpectrogramControls::SpectrogramControls(const QString & title, QWidget * parent)
 	: QDockWidget::QDockWidget(title, parent)
 {
-	QWidget *widget = new QWidget;
-	layout = new QFormLayout;
+	widget = new QWidget(this);
+	layout = new QFormLayout(widget);
 
-	fftSizeSlider = new QSlider(Qt::Horizontal);
+	fftSizeSlider = new QSlider(Qt::Horizontal, widget);
 	fftSizeSlider->setRange(7, 13);
 	fftSizeSlider->setValue(10);
 	layout->addRow(new QLabel(tr("FFT size:")), fftSizeSlider);
 
-	powerMaxSlider = new QSlider(Qt::Horizontal);
+	powerMaxSlider = new QSlider(Qt::Horizontal, widget);
 	powerMaxSlider->setRange(-60, 20);
 	powerMaxSlider->setValue(0);
 	layout->addRow(new QLabel(tr("Power max:")), powerMaxSlider);
 
-	powerRangeSlider = new QSlider(Qt::Horizontal);
+	powerRangeSlider = new QSlider(Qt::Horizontal, widget);
 	powerRangeSlider->setRange(0, 80);
 	powerRangeSlider->setValue(40);
 	layout->addRow(new QLabel(tr("Power range:")), powerRangeSlider);
