ccb104fabe1cd478fb0d8c67a7c8c41d0a0c96a8
Stefan `Sec` Zehl <sec@42.org>
2015-09-20 16:11:21 +0200

    feature: Call external program on snippet

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,4 +1,5 @@
 #include <QtWidgets>
+#include <QRubberBand>
 
 #include "mainwindow.h"
 
@@ -22,6 +23,9 @@ MainWindow::MainWindow()
     connect(dock->powerMinSlider, SIGNAL(valueChanged(int)), &spectrogram, SLOT(setPowerMin(int)));
 }
 
+QRubberBand *rubberBand=NULL;
+QPoint mystart;
+
 bool MainWindow::eventFilter(QObject * /*obj*/, QEvent *event)
 {
     if (event->type() == QEvent::Wheel) {
@@ -40,7 +44,51 @@ bool MainWindow::eventFilter(QObject * /*obj*/, QEvent *event)
             }
             return true;
         }
-    }
+    }else if (event->type() == QEvent::MouseButtonPress) {
+        QMouseEvent *mouseEvent = (QMouseEvent*)event;
+		if (mouseEvent->buttons() == Qt::LeftButton){
+			mystart = (mouseEvent->pos());
+			if(!rubberBand)
+				rubberBand = new QRubberBand(QRubberBand::Rectangle, scrollArea.viewport());
+			rubberBand->setGeometry(QRect(mystart, mystart));
+			rubberBand->show();
+			return true;
+		}
+    }else if (event->type() == QEvent::MouseMove) {
+        QMouseEvent *mouseEvent = (QMouseEvent*)event;
+		if (mouseEvent->buttons() == Qt::LeftButton){
+			rubberBand->setGeometry(QRect(mystart, mouseEvent->pos()).normalized()); //Area Bounding
+			return true;
+		}
+    }else if (event->type() == QEvent::MouseButtonRelease) {
+        QMouseEvent *mouseEvent = (QMouseEvent*)event;
+		QRect rb= rubberBand->geometry();
+
+		int topSample=spectrogram.lineToSample(scrollArea.verticalScrollBar()->value()+ rb.top());
+		int bottomSample=spectrogram.lineToSample(scrollArea.verticalScrollBar()->value()+ rb.bottom());
+
+		int leftFreq=(int)(scrollArea.horizontalScrollBar()->value() + rb.left())*spectrogram.getSampleRate()/spectrogram.getFFTSize();
+		int rightFreq=(int)(scrollArea.horizontalScrollBar()->value() + rb.right())*spectrogram.getSampleRate()/spectrogram.getFFTSize();
+
+		QStringList command;
+		command << "cut_sample" << spectrogram.getFileName() << QString::number(spectrogram.getSampleRate())
+		<< QString::number(spectrogram.lineToSample(scrollArea.verticalScrollBar()->value()+ rb.top()))
+		<< QString::number(spectrogram.lineToSample(scrollArea.verticalScrollBar()->value()+ rb.bottom()))
+
+		<< QString::number((long int)(scrollArea.horizontalScrollBar()->value() + rb.left())*spectrogram.getSampleRate()/spectrogram.getFFTSize() +spectrogram.getCenterFreq() -spectrogram.getSampleRate()/2)
+		<< QString::number((long int)(scrollArea.horizontalScrollBar()->value() + rb.right())*spectrogram.getSampleRate()/spectrogram.getFFTSize() +spectrogram.getCenterFreq() -spectrogram.getSampleRate()/2)
+		<< QString::number(spectrogram.getCenterFreq())
+		;
+
+		if(system(command.join(" ").toLatin1().data())){
+			QMessageBox msgBox;
+			msgBox.setText("cut_sample call failed.");
+			msgBox.exec();
+		};
+		rubberBand->hide();
+		rubberBand->clearMask();
+		return true;
+	};
     return false;
 }
 
