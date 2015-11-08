7c7ff6edd666a58b7af7c71192416308b9ebe7f5
Mike Walters <mike@flomp.net>
2015-11-08 20:38:26 +0000

    Add generic SampleSource interface

---
+++ samplesource.h
@@ -0,0 +1,32 @@
+/*
+ *  Copyright (C) 2015, Mike Walters <mike@flomp.net>
+ *
+ *  This file is part of inspectrum.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <complex>
+
+class SampleSource
+{
+
+public:
+    virtual ~SampleSource() {};
+
+    virtual bool getSamples(std::complex<float> *dest, off_t start, off_t length);
+    virtual off_t count() = 0;
+};
