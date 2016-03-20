b4bf17d8d570d60fa6096b92b459f0239e10abf1
Mike Walters <mike@flomp.net>
2016-03-20 00:49:24 +0000

    tuner: Add missing include
    Fixes #54

--- tuner.cpp
+++ tuner.cpp
@@ -17,6 +17,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <cstdlib>
 #include "tuner.h"
 
 Tuner::Tuner(QObject * parent) : QObject::QObject(parent)
