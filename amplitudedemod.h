43401be07d0d2a29efeb6947b00e4f7cdf6c1536
Mike Walters <mike@flomp.net>
2016-06-10 19:35:52 +0100

    plots: Don't use GR for amplitude demod

---
+++ amplitudedemod.h
@@ -0,0 +1,29 @@
+/*
+ *  Copyright (C) 2016, Mike Walters <mike@flomp.net>
+ *
+ *  This file is part of inspectrum.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "samplebuffer.h"
+
+class AmplitudeDemod : public SampleBuffer<std::complex<float>, float>
+{
+public:
+    AmplitudeDemod(SampleSource<std::complex<float>> *src);
+    void work(void *input, void *output, int count, off_t sampleid) override;
+};
--- plots.cpp
+++ plots.cpp
@@ -17,9 +17,9 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <amplitudedemod.h>
 #include <gnuradio/top_block.h>
 #include <gnuradio/analog/quadrature_demod_cf.h>
-#include <gnuradio/blocks/complex_to_mag.h>
 #include <gnuradio/blocks/threshold_ff.h>
 #include "grsamplebuffer.h"
 #include "memory_sink.h"
@@ -38,16 +38,9 @@ Plot* Plots::samplePlot(std::shared_ptr<AbstractSampleSource> source)
 
 Plot* Plots::amplitudePlot(std::shared_ptr<AbstractSampleSource> source)
 {
-    gr::top_block_sptr tb = gr::make_top_block("amp");
-    auto memSrc = gr::blocks::memory_source::make(8);
-    auto memSink = gr::blocks::memory_sink::make(4);
-    auto complexToMag = gr::blocks::complex_to_mag::make();
-    tb->connect(memSrc, 0, complexToMag, 0);
-    tb->connect(complexToMag, 0, memSink, 0);
-
     return new TracePlot(
-        std::make_shared<GRSampleBuffer<std::complex<float>, float>>(
-            std::dynamic_pointer_cast<SampleSource<std::complex<float>>>(source).get(), tb, memSrc, memSink
+        std::make_shared<AmplitudeDemod>(
+            std::dynamic_pointer_cast<SampleSource<std::complex<float>>>(source).get()
         )
     );
 }
